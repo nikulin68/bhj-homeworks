3d1e6ede7633dddcf862b9ccdd720c32e642df13
Dmitriy <97383552+nikulin68@users.noreply.github.com>
2022-10-07 14:36:50 +0300

    Add files via upload

---
+++ js-features/countdown/task.js
@@ -0,0 +1,14 @@
+function timerStart() {
+    let elem = document.getElementById("timer");
+    let number = parseInt(elem.textContent, 10); // получаем на вход число. Изначально 59
+    elem.textContent = number - 1; // предыдущее число - 1
+    
+    console.log(number);
+
+    if (number == 0) {
+        alert('Вы победили в конкурсе!');
+        location.reload();
+    } 
+}
+
+setInterval(timerStart, 1000);
