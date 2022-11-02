a78cae1049fe947d9824ae0af4965d0af3408c1f
Dmitriy <97383552+nikulin68@users.noreply.github.com>
2022-11-02 17:56:06 +0300

    Delete task.js

--- task.js
+++
@@ -1,9 +0,0 @@
-const reveal = document.querySelectorAll('.reveal');
-
-reveal.forEach((item) => {
-  window.addEventListener('scroll', (event) => {
-    if(window.innerHeight > item.getBoundingClientRect().top) {
-      item.classList.add('reveal_active');
-    }
-  })
-})
